6b4d77d8ded9a5ec8f70874fa28b1125e4c778af
Petr Kozelka <pkozelka@gmail.com>
2020-01-16 13:26:24 +0100

    sample graph construction

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use onnx_helpers::*;
+use onnx_pb::tensor_proto::DataType;
+
+fn main() {
+
+    let x_input: onnx_pb::ValueInfoProto = make_tensor_value_info("X", DataType::Float, vec![1, 10], None);
+    let inputs = vec![
+        x_input
+    ];
+
+    let node1 = make_node(
+        "abc",
+        vec!["X"],
+        vec!["Z"],
+        None,
+        None,
+        None,
+        vec![make_attribute("axes", vec![1i64].into())],
+    );
+
+    let graph = make_graph(
+        vec![node1],
+        "name",
+//        inputs,
+        vec![],
+        vec![],
+        vec![],
+        None,
+    );
+
+    let model = make_model(graph,
+                           None,
+                           None,
+                           None,
+                           None,
+                           None,
+                           None,
+                           None,
+    );
+}
